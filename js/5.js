e56b7475016dcbfab91f8e4ebd465a428acb1714
Katsuya Yoshizumi <kokono1220@gmail.com>
2016-09-14 19:36:32 +0900

    s

--- js/5.js
+++ js/5.js
@@ -1,16 +1,22 @@
-var WebGL;
-(function(WebGL) {
+var GPF_STAMP_RALLY;
+(function(GPF_STAMP_RALLY) {
   'use strict';
-  var TEST = (function() {
-    function TEST() {
+  var coinAnime = (function() {
+    function coinAnime() {
       var _this = this;
 
       // 空間をつくる
       this.scene = new THREE.Scene();
-      this.camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 1, 10000);
-      this.renderer = new THREE.WebGLRenderer;
+      this.camera = new THREE.PerspectiveCamera(20, 600 / 600, 1, 10000);
 
-      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      var renderer_options = {alpha: true};
+      if ( this.webglAvailable() && false ) {
+        this.renderer = new THREE.WebGLRenderer(renderer_options);
+      } else {
+        this.renderer = new THREE.CanvasRenderer(renderer_options);
+      }
+      this.renderer.setPixelRatio(window.devicePixelRatio || 1)
+      this.renderer.setSize(600, 600);
       document.body.appendChild(this.renderer.domElement);
 
       // ライト
@@ -20,14 +26,15 @@ var WebGL;
       this.ambient = new THREE.AmbientLight(0x999999);
 
       // XYZ軸を表示する
+      /*
       this.axis = new THREE.AxisHelper(1000);
-
       this.controls = new THREE.OrbitControls(this.camera);
+      */
 
       this.light.position.set(0, 100, 30);
       this.camera.position.set(0, 0, 200);
 
-      this.scene.add(this.light, this.ambient, this.axis);
+      this.scene.add(this.light, this.ambient);
 
       // コインをつくる
       var coin_sides_geo = new THREE.CylinderGeometry(10.0, 10.0, 1.0, 100.0, 10.0, true);
@@ -35,9 +42,9 @@ var WebGL;
 
       // テクスチャーの読み込み
       var texloader = new THREE.TextureLoader();
-      var coin_sides_texture = texloader.load("/img/coin_side.jpg"); // side.jpg
-      var coin_cap_texture_top = texloader.load("/img/coin_top.png"); // top.jpg
-      var coin_cap_texture_bottom = texloader.load("/img/coin_bottom.jpg"); // bottom.jpg
+      var coin_sides_texture = texloader.load("./img/coin_side.jpg"); // side.jpg
+      var coin_cap_texture_top = texloader.load("./img/coin_top.png"); // top.jpg
+      var coin_cap_texture_bottom = texloader.load("./img/coin_bottom.jpg"); // bottom.jpg
 
 
       var coin_sides_mat = new THREE.MeshLambertMaterial({ map: coin_sides_texture });
@@ -65,7 +72,7 @@ var WebGL;
       this.coin.rotation.y -= Math.PI * 0.3;
 
       // グロー効果の追加
-      var coin_grow_texture = texloader.load("img/glow.png");
+      var coin_grow_texture = texloader.load("./img/glow.png");
       var coin_grow_mat = new THREE.MeshBasicMaterial( 
       { 
         map: coin_grow_texture,
@@ -105,32 +112,73 @@ var WebGL;
       this.scene.add( ball );
       */
 
-      var spark_texture = texloader.load("img/spark.png");
-      var spark_mat = new THREE.MeshBasicMaterial( { 
-        map: spark_texture,
-        color: 0xffffff,
-        blending: THREE.AdditiveBlending,
-        side: THREE.DoubleSide,
-        transparent: true,
-        opacity: 0.8
-      } );
-      var spark_geo = new THREE.CircleGeometry (4, 4, 0);
-      this.spark = new THREE.Mesh(spark_geo, spark_mat);
-      this.spark.position.set( 10, 0, -40 );
-      this.spark.material.color.setHSL( 1, 1, 1 ); 
-      this.scene.add( this.spark );
-      
-      TweenMax
-        .to(spark_mat, 0.3, {
-          opacity: 1 * Math.random(),
-          startAt: { opacity: 0 },
-          repeat: -1,
-          yoyo: true,
-          onUpdate: function(material, hsl) {
-            material.color.setHSL(hsl.h + (this.progress() / 5), hsl.s, hsl.l);
-          },
-          onUpdateParams: [ spark_mat, spark_mat.color.getHSL() ],
-        }, 0.4 + Math.random());
+      this.sparks = [];
+      var spark_num = 3;
+      function set_random_spark_pos (pos) {
+        var radian = (360 * Math.random()) * Math.PI / 180;
+        var radius = 10;
+        pos.x = Math.cos(radian) * radius + 0.3 * Math.random();
+        pos.y = Math.sin(radian) * radius + 0.3 * Math.random();
+      };
+
+      for (var i = 0; i < spark_num; i++) {
+
+        var spark_texture = texloader.load("img/spark.png");
+        var spark_mat = new THREE.MeshBasicMaterial( { 
+          map: spark_texture,
+          color: 0xffffff,
+          blending: THREE.AdditiveBlending,
+          side: THREE.DoubleSide,
+          transparent: true,
+          opacity: 0
+        } );
+        var spark_geo = new THREE.CircleGeometry (2.5, 2.5, 0);
+        this.sparks.push( new THREE.Mesh(spark_geo, spark_mat) );
+        set_random_spark_pos(this.sparks[i].position);
+        this.sparks[i].position.z = -40;
+        this.scene.add( this.sparks[i] );
+
+        TweenMax
+          .to(this.sparks[i].material, 0.4, {
+            //delay: Math.random()*(2-0.3)+0.6,
+            opacity: Math.random()*(1-0.3)+0.3,
+            startAt: { opacity: 0 },
+            repeat: -1,
+            yoyo: true,
+            onUpdate: function(mesh) {
+              if (mesh.material.opacity < 0.09) {
+
+                set_random_spark_pos(mesh.position);
+
+                var scale = Math.random()*(1-0.3)+0.3;
+                mesh.scale.x = scale;
+                mesh.scale.y = scale;
+              }
+            },
+            onUpdateParams: [ this.sparks[i] ],
+          }, 0.4 + Math.random());
+
+        TweenMax
+          .to(this.sparks[i].material, 0.4, {
+            delay: Math.random()*(2-0.3)+0.6,
+            opacity: Math.random()*(1-0.3)+0.3,
+            startAt: { opacity: 0 },
+            repeat: -1,
+            yoyo: true,
+            onUpdate: function(mesh) {
+              if (mesh.material.opacity < 0.09) {
+
+                set_random_spark_pos(mesh.position);
+
+                var scale = Math.random()*(1-0.3)+0.3;
+                mesh.scale.x = scale;
+                mesh.scale.y = scale;
+              }
+            },
+            onUpdateParams: [ this.sparks[i] ],
+          }, 0.4 + Math.random());
+
+      }
 
       TweenMax
         .to(this.coin.rotation, 1.6, {
@@ -156,7 +204,7 @@ var WebGL;
       this.render();
     };
 
-    TEST.prototype.render = function() {
+    coinAnime.prototype.render = function() {
       var _this = this;
 
       function animation(time) {
@@ -165,12 +213,23 @@ var WebGL;
         _this.coin_grow.rotation.z += 0.01;
 
         _this.renderer.render(_this.scene, _this.camera);
-      }
-
+      };
       animation();
     };
 
-    return TEST;
+    coinAnime.prototype.webglAvailable = function() {
+      try {
+        var canvas = document.createElement( 'canvas' );
+        return !!( window.WebGLRenderingContext && (
+          canvas.getContext( 'webgl' ) ||
+          canvas.getContext( 'experimental-webgl' ) )
+        );
+      } catch ( e ) {
+        return false;
+      }
+    };
+
+    return coinAnime;
   }());
-  WebGL.TEST = TEST;
-})(WebGL || (WebGL = {}));
+  GPF_STAMP_RALLY.coinAnime = coinAnime;
+})(GPF_STAMP_RALLY || (GPF_STAMP_RALLY = {}));
